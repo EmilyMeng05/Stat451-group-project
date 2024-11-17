02eb3398f36d2e5c919cf0c7598533db971f1945
jinx-song <89862960+jinx-song@users.noreply.github.com>
2024-11-17 17:30:40 -0500

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,3 +1,8 @@
+library(leaflet)
+library(DT)
+
+load("endangeredFinal.RData")
+
 fluidPage(
   titlePanel("Stats 451 example"),
   
@@ -5,6 +10,16 @@ fluidPage(
     
     sidebarPanel(
       
-    )
+      
+      selectInput("country",
+                  "Select country",
+                  distinct(endangered, endangered$`Region/Country/Area`),
+                  "United States of America"
+      )
+      
+    ),
+    
+    mainPanel()
+    
   )
 )
