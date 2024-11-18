15d1213879da42520dbf5afc48f7b3b60c4942f2
EmilyMeng05 <160436343+EmilyMeng05@users.noreply.github.com>
2024-11-17 18:03:49 -0800

    Update ui.R
    update~

--- ui.R
+++ ui.R
@@ -1,34 +1,38 @@
-library(leaflet)
-library(DT)
-
-load("endangeredFinal.RData")
+library(shiny)
 
 fluidPage(
-  titlePanel("Stats 451 example"),
+  
+  titlePanel("Threatened Species Analysis (Stats 451)"),
   
   sidebarLayout(
     
-   
     sidebarPanel(
       radioButtons(
         inputId = "viewOption",
-        label = "Choose the view:",
+        label = "What question are you looking for?:",
         choices = list(
           "Overall Trend for Species Changing" = "trend",
           "Top 5 Countries with the Highest Total Amount of Threatened Species" = "top5",
           "Top 5 Countries trend for Species Changing" = "5trend",
-          "Highest Lost of Life" = "lost",
+          "Highest Lost of Life Graph" = "lost",
           "Average Number Across the World" = "average"
         ),
         selected = "trend"
+      ),
+      
+      # Add a checkbox for Species Type
+      checkboxGroupInput(
+        inputId = "speciesType", 
+        label = "Filter by Species Type", 
+        choices = c("Total", "Vertebrates", "Invertebrates", "Plants"),
+        selected = c("Total", "Vertebrates", "Invertebrates", "Plants")  # Default to all species types
       )
     ),
     
     mainPanel(
-      h3("Dynamic Visualization"),
+      h3("Visualization graph"),
       uiOutput("dynamicTitle"),
       plotOutput("selectedGraph") 
     )
-    
   )
 )
