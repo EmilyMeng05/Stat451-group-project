e313bd6e154adcc36bfb2b5d09ee9a0c812deb74
EmilyMeng05 <160436343+EmilyMeng05@users.noreply.github.com>
2024-11-17 18:04:19 -0800

    Update server.R

--- server.R
+++ server.R
@@ -1,3 +1,4 @@
+# library
 library(shiny)
 library(dplyr)
 library(forcats)
@@ -7,23 +8,34 @@ library(tidyverse)
 library(stringr)
 
 shinyServer(function(input, output) {
-  
+  # from the old project
+  # rename the dataset
   df <- df %>%
     rename(CountryName = 2) %>%
     mutate(SpeciesType = 
              unlist(str_extract_all(Series, '(?<=:\\s).*(?=\\s\\()')))
   
   output$selectedGraph <- renderPlot({
+    # first graph
     if (input$viewOption == "trend") {
-      # Plot for "Overall Trend"
-      ggplot(df, aes(x = Year, y = Value, color = SpeciesType)) +
-        geom_line() +
-        labs(title = "Overall Trend of Threatened Species Over Time",
-             x = "Year",
-             y = "Number of Threatened Species") +
-        theme_minimal()
+      df_filtered <- df %>%
+        filter(SpeciesType %in% input$speciesType)
+      
+      df_total_species <- df_filtered %>%
+        group_by(Year, SpeciesType) %>%
+        summarize(TotalValue = sum(Value, na.rm = TRUE), .groups = 'drop')
+      
+      # the first graph code
+      ggplot(df_total_species, aes(x = Year, y = TotalValue, color = SpeciesType)) +
+        geom_line() +  
+        labs(x = "Year", y = "Amount", title = "Total Number of Endangered Species Over the Years By Group") +
+        theme_minimal() +
+        theme(legend.title = element_blank()) + 
+        scale_y_continuous(limits = c(0, 60000), breaks = seq(0, 40000, by = 10000)) +  # Zoom in on y-axis
+        theme(legend.position = "right") 
+      
     } else if (input$viewOption == "top5") {
-      # Data for "Top 5 Countries"
+      # get the top 5
       df_country_total_top <- df %>%
         filter(SpeciesType == "Total", !is.na(Value)) %>%
         group_by(CountryName) %>%
@@ -31,7 +43,7 @@ shinyServer(function(input, output) {
         arrange(desc(TotalThreatened)) %>%
         slice_head(n = 5)
       
-      # Plot for "Top 5 Countries"
+      # plot the graph
       ggplot(df_country_total_top, aes(x = fct_reorder(CountryName, TotalThreatened), y = TotalThreatened)) +
         geom_bar(stat = "identity", fill = "coral") +
         geom_text(aes(label = TotalThreatened),
@@ -44,15 +56,35 @@ shinyServer(function(input, output) {
              y = "Total Amount of Threatened Species") +
         coord_flip() +
         theme_minimal()
-    }else if (input$viewOption == "5trend") {
-      # code needed
-      ggplot() + labs(title = "Feature under development")
+    } else if (input$viewOption == "5trend") {
+      # List of top 5 countries
+      top_5_countries <- c("Ecuador", "Madagascar", "United States of America","Indonesia", "Malaysia")
+      
+      # Filter to not include total and also only within top 5 country
+      df_top_5_countries <- df %>%
+        filter(SpeciesType %in% c("Vertebrate", "Invertebrate", "Plants"), 
+               CountryName %in% top_5_countries, 
+               !is.na(Value)) 
+      
+      # to make sure the legand are lined based on the order of 5 countries
+      df_top_5_countries$CountryName <-
+        factor(df_top_5_countries$CountryName,
+               levels = top_5_countries)
+      
+      ggplot(df_top_5_countries, aes(x = Year, y = Value, color = CountryName, group = CountryName)) +
+        geom_line() +
+        geom_point() +
+        labs(title = "Threatened species amount Trends Over Time for Top 5 Countries",
+             x = "Year",
+             y = "Total amount of Threatened species value") +
+        scale_x_continuous(breaks = unique(df_top_5_countries$Year)) +
+        theme_minimal() +
+        theme(legend.title = element_blank(), 
+              legend.position = "right") 
     } else if (input$viewOption == "lost") {
-      # code ndeede
-      ggplot() + labs(title = "Feature under development")
+      # code needed
     } else if (input$viewOption == "average") {
-      # coded needed
-      ggplot() + labs(title = "Feature under development")
+      # code needed
     }
     
   })
